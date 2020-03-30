c497597a0591e4f7c10a9fe83fca6075f56d1f72
lcastro92 <lucas.castro.0307@gmail.com>
2020-03-30 15:46:51 -0300

    Ready papa

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -16,6 +16,7 @@ public class JavaApplication6 {
      */
     public static void main(String[] args) {
         System.out.println("Prueba"    );
+        System.out.println("Modificado perra");
     }
     
 }
